03150f930e3292882f1478773d1eb01e10827991
yyq <2410985372@qq.com>
2021-11-07 19:27:49 +0800

    提交代码

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import Nav from "@/components/Nav.vue";
+import Layout from "@/components/Layout.vue";
+import Icon from "@/components/Icon.vue";
+// import tagListModel from '@/models/tagListModel';
+// import recordListModel from '@/models/recordListModel';
+
+Vue.config.productionTip = false;
+
+Vue.component("Nav", Nav);
+Vue.component("Layout", Layout);
+Vue.component("Icon", Icon);
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
+
+window.onload = function () {
+  setTimeout(function () {
+    window.scrollTo(0, 10000);
+  }, 0);
+};
---
+++ src/views/Money.vue
@@ -0,0 +1,65 @@
+<template>
+  <Layout class-prefix="layout">
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+    <Tabs :data-source="recordTypeList" :value.sync="record.type" />
+    <div class="notes">
+      <FormItem
+        field-name="备注"
+        placeholder="在这里输入备注"
+        :value.sync="record.notes"
+      />
+    </div>
+    <Tags @update:value="record.tags = $event" />
+  </Layout>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import NumberPad from "@/components/Money/NumberPad.vue";
+import FormItem from "@/components/Money/FormItem.vue";
+import Tags from "@/components/Money/Tags.vue";
+import { Component } from "vue-property-decorator";
+import Tabs from "@/components/Tabs.vue";
+import recordTypeList from "@/constants/recordTypeList";
+@Component({
+  components: { Tabs, Tags, FormItem, NumberPad },
+})
+export default class Money extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+  recordTypeList = recordTypeList;
+  record: RecordItem = {
+    tags: [],
+    notes: "",
+    type: "-",
+    amount: 0,
+  };
+  created() {
+    this.$store.commit("fetchRecords");
+  }
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
+  }
+  saveRecord() {
+    if (!this.record.tags || this.record.tags.length === 0) {
+      return window.alert("最少选一个标签吧亲~~");
+    }
+    this.$store.commit("createRecord", this.record);
+    if (this.$store.state.createRecordError === null) {
+      window.alert("已经保存啦亲~~");
+      this.record.notes = "";
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep .layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+.notes {
+  padding: 12px 0;
+}
+</style>
